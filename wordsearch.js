b3c6a315a105b7605f26f1cc7456ebbf7f1c41bc
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-10-22 22:07:16 -0400

    completed and inplemented a word searcher solver

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
 const wordSearch = (letters, word) => { 
+
+    if(letters.length < 1 || word.length < 1) return false;
+
     const horizontalJoin = letters.map(ls => ls.join(''))
+
+    const verticalJoin = letters[0].map((col, index) => letters.map((row) => row[index]));
+
+    const verticalSwitch = verticalJoin.map((ls) => ls.join(''));
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) 
+        return true;
+
+     else {
+
+        for(l of verticalSwitch) {
+            if (l.includes(word)) 
+            return true;
+         }
+       }
     }
+    return false;
 }
 
 module.exports = wordSearch
